fa3a892309653697a19da162da7f8cd670315bd5
NickLilland <nlilla1@live.spcollege.edu>
2024-08-21 21:24:37 -0400

    Initial Commit

---
+++ HITFit/HITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HITFitApp.swift
+//  HITFit
+//
+//  Created by Nick Lilland on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
